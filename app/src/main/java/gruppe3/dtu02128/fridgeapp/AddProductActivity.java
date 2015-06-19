1e998d847f5e7cb50125932665e23cfd01fbd12b
Mathias Mortensen <tympanix@gmail.com>
2015-06-19 14:52:04 +0200

    Bug fix, register DB

--- app/src/main/java/gruppe3/dtu02128/fridgeapp/AddProductActivity.java
+++ app/src/main/java/gruppe3/dtu02128/fridgeapp/AddProductActivity.java
@@ -69,7 +69,7 @@ public class AddProductActivity extends Activity implements DatePickerDialog.OnD
             @Override
             public void onClick(View v) {
                 if (v.getId() == R.id.scan_button) {
-                    if (barcode != null || !barcode.isEmpty()){
+                    if (barcode != null){
                         barcode = null;
                         mScanButton.setText(R.string.scan_button);
                         mItemName.setText(null);
